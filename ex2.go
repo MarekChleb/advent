a6ebc6f535b30650ae5df971eb9a80c5b873d6e1
Marek Wawreniuk <marek.wawreniuk@websensa.com>
2020-12-02 21:17:34 +0100

    feat: add main prototype

---
+++ ex2.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type pass struct {
+	min, max int
+	letter   rune
+	p        string
+}
+
+func ex2a() {
+
+	count := 0
+
+	for _, l := range lines {
+		lEls := strings.Split(l, " ")
+		lNs := strings.Split(lEls[0], "-")
+		min, _ := strconv.ParseInt(lNs[0], 10, 64)
+		max, _ := strconv.ParseInt(lNs[1], 10, 64)
+
+		var letter rune
+		for i, v := range lEls[1] {
+			if i == 0 {
+				letter = v
+			}
+		}
+
+		pp := pass{
+			min:    int(min),
+			max:    int(max),
+			letter: letter,
+			p:      lEls[2],
+		}
+		fmt.Println(pp)
+
+		if pp.check() {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
+
+func ex2b() {
+
+	count := 0
+
+	for _, l := range lines {
+		lEls := strings.Split(l, " ")
+		lNs := strings.Split(lEls[0], "-")
+		min, _ := strconv.ParseInt(lNs[0], 10, 64)
+		max, _ := strconv.ParseInt(lNs[1], 10, 64)
+
+		var letter rune
+		for i, v := range lEls[1] {
+			if i == 0 {
+				letter = v
+			}
+		}
+
+		pp := pass{
+			min:    int(min),
+			max:    int(max),
+			letter: letter,
+			p:      lEls[2],
+		}
+		fmt.Println(pp)
+
+		if pp.check2() {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
+
+func (p pass) check() bool {
+	count := 0
+	for _, s := range p.p {
+		if s == p.letter {
+			count++
+		}
+	}
+	return count >= p.min && count <= p.max
+}
+
+func (p pass) check2() bool {
+	ok := false
+	for i, s := range p.p {
+		if i+1 == p.min {
+			ok = s == p.letter
+		}
+		if i+1 == p.max {
+
+			return ok != (s == p.letter)
+		}
+	}
+	return false
+}
---
+++ ex3.go
@@ -0,0 +1,9 @@
+package main
+
+func ex3a() {
+
+}
+
+func ex3b() {
+
+}
