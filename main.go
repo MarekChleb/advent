3174e662d5e9695138c9e55d2360364384be6e23
Marek Wawreniuk <marek.wawreniuk@websensa.com>
2020-12-07 09:41:19 +0100

    feat: ex6

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 
 	"github.com/mw332637/advent/ioutils"
-	"github.com/mw332637/advent/solutions/ex4"
+	"github.com/mw332637/advent/solutions/ex6"
 )
 
 var lines []string
 
 func main() {
-	fileNames := ioutils.ReadArgs(ex4.Path)
+	fileNames := ioutils.ReadArgs(ex6.Path)
 
 	for _, name := range fileNames {
 		lines = ioutils.LoadFileLines(name)
@@ -18,10 +18,10 @@ func main() {
 		fmt.Println("File: ", name)
 
 		fmt.Print("A: ")
-		ex4.A(lines)
+		ex6.A(lines)
 
 		fmt.Print("B: ")
-		ex4.B(lines)
+		ex6.B(lines)
 
 		fmt.Println()
 	}
--- solutions/ex6/ex6.go
+++ solutions/ex6/ex6.go
@@ -9,11 +9,48 @@ const Path = "./solutions/ex6"
 func A(lines []string) {
 	count := 0
 
+	var m = map[rune]struct{}{}
+
+	for _, l := range lines {
+		if l == "" {
+			for range m {
+				count++
+			}
+			m = map[rune]struct{}{}
+			continue
+		}
+
+		for _, c := range l {
+			m[c] = struct{}{}
+		}
+	}
+
 	fmt.Println(count)
 }
 
 func B(lines []string) {
 	count := 0
+
+	var m = map[rune]int{}
+	cc := 0
+
+	for _, l := range lines {
+		if l == "" {
+			for _, v := range m {
+				if v == cc {
+					count++
+				}
+			}
+			m = map[rune]int{}
+			cc = 0
+			continue
+		}
+
+		for _, c := range l {
+			m[c] = m[c] + 1
+		}
+		cc++
+	}
 
 	fmt.Println(count)
 }
